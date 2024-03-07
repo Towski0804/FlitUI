0a78a9feb4e8680e683482db6489180b8020c480
Towski0804 <superchao0206@gmail.com>
2024-03-07 15:15:17 +0000

    Button component fully functioning now

--- src/components/ButtonPage.vue
+++ src/components/ButtonPage.vue
@@ -1,11 +1,65 @@
 <template>
-  <div>the demo of Button</div>
-  <h1>Example 1</h1>
+  <h1>the demo of Button</h1>
+  <h2>Button Kind</h2>
   <div>
     <Button>default kind</Button>
     <Button kind="button">button</Button>
     <Button kind="link">link</Button>
     <Button kind="text">text</Button>
+  </div>
+  <hr />
+  <h2>Button Size</h2>
+  <div>
+    <h3>kind=button(default)</h3>
+    <Button size="big">Big</Button>
+    <Button>Normal</Button>
+    <Button size="small">Small</Button>
+  </div>
+  <div>
+    <h3>kind=link</h3>
+    <Button kind="link" size="big">Big</Button>
+    <Button kind="link">Normal</Button>
+    <Button size="small" kind="link">Small</Button>
+  </div>
+  <div>
+    <h3>kind=text</h3>
+    <Button size="big" kind="text">Big</Button>
+    <Button kind="text">Normal</Button>
+    <Button size="small" kind="text">Small</Button>
+  </div>
+  <hr />
+  <h2>Button Level</h2>
+  <div>
+    <div>
+      <h3>kind=button(default)</h3>
+      <Button level="main">Main Button</Button>
+      <Button>Normal Button</Button>
+      <Button level="danger">Danger Button</Button>
+    </div>
+    <div>
+      <h3>kind=link</h3>
+      <Button kind="link" level="main">Main Link</Button>
+      <Button kind="link">Normal Link</Button>
+      <Button kind="link" level="danger">Danger Link</Button>
+    </div>
+    <div>
+      <h3>kind=text</h3>
+      <Button kind="text" level="main">Main Text</Button>
+      <Button kind="text">Normal Text</Button>
+      <Button kind="text" level="danger">Danger Text</Button>
+    </div>
+  </div>
+  <hr />
+  <h2>Button Disabled</h2>
+  <div>
+    <Button disabled>Disabled Button</Button>
+    <Button kind="link" disabled>Disabled Link</Button>
+    <Button kind="text" disabled>Disabled Text</Button>
+  </div>
+  <hr />
+  <div>
+    <h2>Loading</h2>
+    <Button loading>loading</Button>
   </div>
 </template>
 <script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,24 +1,59 @@
 <template>
-  <button class="flit-button" :class="`kind-${kind}`">
+  <button class="flit-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="flit-loadingSign"></span>
     <slot />
   </button>
 </template>
 <script lang="ts">
+import { computed } from 'vue'
+
 export default {
   props: {
     kind: {
       type: String,
       default: 'button'
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    },
+    level: {
+      type: String,
+      default: 'normal'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props) {
+    const { kind, size, level } = props
+    const classes = computed(() => {
+      return {
+        [`flit-kind-${kind}`]: true,
+        [`flit-size-${size}`]: true,
+        [`flit-level-${level}`]: true
+      }
+    })
+    return {
+      classes
     }
   }
 }
 </script>
+
 <style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
+$red: red;
 $radius: 4px;
+$grey: grey;
 .flit-button {
   box-sizing: border-box;
   height: $h;
@@ -33,6 +68,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -47,7 +83,7 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
-  &.kind-link {
+  &.flit-kind-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
@@ -56,12 +92,109 @@ $radius: 4px;
       color: lighten($blue, 10%);
     }
   }
-  &.kind-text {
+  &.flit-kind-text {
     border-color: transparent;
     box-shadow: none;
     &:hover,
     &:focus {
       background: #f5f5f5;
+    }
+  }
+  &.flit-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.flit-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.flit-kind-button {
+    &.flit-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.flit-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.flit-kind-link {
+    &.flit-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.flit-kind-text {
+    &.flit-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.flit-level-normal {
+      color: $color;
+      &:hover,
+      &:focus {
+        color: darken($color, 10%);
+      }
+    }
+    &.flit-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.flit-kind-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.flit-kind-link,
+  &.flit-kind-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+  > .flit-loadingSign {
+    width: 16px;
+    height: 16px;
+    border: 2px solid navy;
+    border-top-color: transparent;
+    border-radius: 50%;
+    animation: flit-spin 1s linear infinite;
+    margin-right: 3px;
+  }
+  @keyframes flit-spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
     }
   }
 }

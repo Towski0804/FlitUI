78781d96ee3e745ec9e96a784e6b46e7b0e24270
Towski0804 <superchao0206@gmail.com>
2024-03-09 16:33:00 +0800

    Dialogue component now support 'visible' attribute

--- src/components/DialoguePage.vue
+++ src/components/DialoguePage.vue
@@ -1,12 +1,27 @@
 <template>
-  <Dialogue>888</Dialogue>
+  <div>Dialogue Component</div>
+  <Button @click="toggle">Open Dialogue</Button>
+  <Dialogue :visible="visible">888</Dialogue>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
+import Button from '../lib/Button.vue'
 import Dialogue from '../lib/Dialogue.vue'
 export default {
   components: {
+    Button,
     Dialogue
+  },
+  setup() {
+    const visible = ref(false)
+    const toggle = () => {
+      visible.value = !visible.value
+    }
+    return {
+      visible,
+      toggle
+    }
   }
 }
 </script>

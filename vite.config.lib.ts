8f72d7d195236930b20141d0e6c1bea296c17596
Towski0804 <superchao0206@gmail.com>
2024-03-25 05:45:02 +0000

    Update build configurations

---
+++ vite.config.lib.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'src/lib/index.ts',
+      name: 'FlitUI',
+      fileName: 'flit-ui',
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
